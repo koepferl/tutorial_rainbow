b581aeae7a9d52f90fdc6da60861dea34abbd8d9
koepferl <koepferl@mpia.de>
2012-10-29 13:47:26 +0100

    colorplot is woring, without colorbar

--- plot_sed.py
+++ plot_sed.py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
 
 from hyperion.model import ModelOutput
 from hyperion.util.constants import pc
@@ -17,21 +18,32 @@ ax = fig.add_subplot(1, 1, 1)
 # arrays, and indicate the position from the end. So to get the SED in the
 # largest aperture, we set aperture=-1.
 
-wav, nufnu = m.get_sed(inclination='all', aperture=-1, distance=300 * pc)
-lines=ax.loglog(wav,nufnu.transpose(),cmap=plt.cm.gist_heat)
-fig.colorbar(lines)
+f = np.linspace(0, 255, 10)
+print f
+j = [0,1,2,3,4,5,6,7,8,9]
+for k in range(len(j)):
+    print 'color number', f[k]
+    print 'integer', int(f[k])
+    g = plt.cm.gist_heat(int(f[k]))
+    print 'color vector', g
+    i = j[k]
+    print 'incination', i
+    
+    wav, nufnu = m.get_sed(inclination=i, aperture=-1, distance=300 * pc)
+    lines=ax.loglog(wav,nufnu.transpose())
+    #fig.colorbar(lines)
 
-# Plot the SED. The loglog command is similar to plot, but automatically
-# sets the x and y axes to be on a log scale.
-ax.loglog(wav, nufnu)
+    # Plot the SED. The loglog command is similar to plot, but automatically
+    # sets the x and y axes to be on a log scale.
+    ax.loglog(wav, nufnu,color=g)
 
 # Add some axis labels (we are using LaTeX here)
 ax.set_xlabel(r'$\lambda$ [$\mu$m]')
 ax.set_ylabel(r'$\lambda F_\lambda$ [ergs/s/cm$^2$]')
 
 # Set view limits
-ax.set_xlim(0.1, 5000.)
-ax.set_ylim(1.e-15, 2.e-10)
+ax.set_xlim(0.1, 2000.)
+ax.set_ylim(1.e-15, 2.e-09)
 
 # Write out the plot
 fig.savefig('sedall.png')
